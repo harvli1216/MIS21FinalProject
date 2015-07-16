fe25a1537cd09c34381e0cc32977ad9e5bc0a886
harvli1216 <harvli1216@gmail.com>
2015-07-16 15:04:45 +0800

    ako
    ako na lang

--- MIS21FinalProj/app/controllers/application_controller.rb
+++ MIS21FinalProj/app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   	protected
   	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender) }2. Open you application_controller.rb
+  		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender) }
 
   	end
 end
