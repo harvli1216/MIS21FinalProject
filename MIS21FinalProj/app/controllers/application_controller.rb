68f417c69bd00a49255d06e3eaef3e80a24fd6d4
harvli1216 <harvli1216@gmail.com>
2015-07-16 16:40:10 +0800

    Remove extra spaces
    sa app controller

--- MIS21FinalProj/app/controllers/application_controller.rb
+++ MIS21FinalProj/app/controllers/application_controller.rb
@@ -6,6 +6,5 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   	protected
   	def configure_permitted_parameters
   		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender) }
-
   	end
 end
