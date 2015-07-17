f6566edcac68db138bb068c089756d799a31bf51
harvli1216 <harvli1216@gmail.com>
2015-07-17 13:27:37 +0800

    Table and shiz

--- MIS21FinalProj/app/models/user.rb
+++ MIS21FinalProj/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+validates :birthday, presence: true
+validates :gender, presence: true
 end
