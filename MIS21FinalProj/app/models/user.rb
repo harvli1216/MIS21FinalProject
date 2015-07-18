5c1b6d8f6672c9cb8ba11240f52bfac69949062f
p-era <pau_era@yahoo.com>
2015-07-18 18:38:28 +0800

    gem gravtastic

--- MIS21FinalProj/app/models/user.rb
+++ MIS21FinalProj/app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 has_many :votes
 validates :gender, presence: true
+include Gravtastic
+gravtastic
 end
